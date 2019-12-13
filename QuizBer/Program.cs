3947b7044c8a99f25ae4452158be026f4ad47927
Leigh Ann Majkowski <LeighAnn.Majkowski@vu.com>
2019-12-13 15:51:45 -0600

    Initial commit

---
+++ QuizBer/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var quizQuestions = new List<Question>();
+
+			//var questionOne = new TrueFalseQuestion()
+			//{
+			//	QuestionString = "A smoker is a type of grill.",
+			//	CorrectAnswerBool = true
+			//};
+
+			//var questionTwo = new TrueFalseQuestion()
+			//{
+			//	QuestionString = "The most popular utensil among grill owners is a long-handled spatula.",
+			//	CorrectAnswerBool = false
+			//};
+
+			//var questionThree = new FillInTheBlankQuestion()
+			//{
+
+			//	QuestionString = "In 2009, competitive eating champion Joey Chestnut ate __ hot dogs with buns in 10 minutes!",
+			//	CorrectAnswerString = "68"
+			//};
+
+			//var questionFour = new OrderQuestion(new[]
+			//	{
+			//		"Humans discover fire",
+			//		"The outdoor gas grill was invented",
+			//		"George Foreman grill invented"
+			//	})
+			//{
+			//	QuestionString = "Put the following items in chronological order:",
+
+			//};
+
+			var questionFive = new MultipleChoiceQuestion()
+			{
+				QuestionString = "Which of the following is the correct definition of a grill?",
+				PossibleAnswers = new List<string>()
+				{
+					"A metal framework used for cooking food over an open fire.",
+					"A device for recording visual images in the form of photographs, film, or video signals.",
+					"A wipeable board with a white surface used for teaching or presentations.",
+					"The star around which the earth orbits."
+				},
+				CorrectAnswerIndex = 0
+			};
+
+			//quizQuestions.Add(questionOne);
+
+			//quizQuestions.Add(questionTwo);
+
+			//quizQuestions.Add(questionThree);
+
+			//quizQuestions.Add(questionFour);
+
+			quizQuestions.Add(questionFive);
+
+			Console.WriteLine("Welcome to QuizBer!");
+			Console.WriteLine();
+			Console.WriteLine("Running 'Basic Grill Quiz'");
+			Console.WriteLine();
+			Console.WriteLine("Press Enter to start the quiz!");
+			Console.ReadLine();
+
+			Console.Clear();
+
+			foreach(var question in quizQuestions)
+			{
+				Console.WriteLine(question.QuestionTypeDisplay);
+				Console.WriteLine();
+				foreach(var line in question.DisplayQuestion())
+				{
+					Console.WriteLine(line);
+				}
+				Console.WriteLine("----------------------------");
+				Console.WriteLine();
+
+				string[] response = new string[question.NumRequiredAnswers];
+				for(int i = 0; i < question.NumRequiredAnswers; i++)
+				{
+					Console.Write(">> ");
+					response[i] = Console.ReadLine();
+				}
+
+				for (var i = 0; i < question.NumRequiredAnswers; i++)
+				{
+					
+				}
+
+				if (question.AttemptAnswer(response))
+				{
+					Console.WriteLine("That is correct");
+					Console.WriteLine();
+				}
+				else
+				{
+					Console.WriteLine("Sorry, that is incorrect.");
+					Console.WriteLine();
+				}
+
+				Console.WriteLine();
+				Console.WriteLine("Press enter to continue ...");
+				Console.ReadLine();
+				Console.Clear();
+			}
+
+			Console.ReadLine();
+
+		}
+
+	}
+}
---
+++ QuizBer/Question.cs
@@ -0,0 +1,166 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBer
+{
+	public abstract class Question
+	{
+		public abstract string QuestionTypeDisplay { get; }
+		public string QuestionString { get; set; }
+		public int NumRequiredAnswers { get; set; } = 1;
+		public abstract bool AttemptAnswer(string[] attemptedAnswer);
+		public virtual List<string> DisplayQuestion()
+		{
+			return new List<string>()
+			{
+				QuestionString,
+			};
+		}
+	}
+
+	public class TrueFalseQuestion : Question
+	{
+		public override string QuestionTypeDisplay { get; } = "True or False Question";
+		public bool CorrectAnswerBool { get; set; }
+		public override bool AttemptAnswer(string[] attemptedAnswer)
+		{
+			if (attemptedAnswer[0].ToLower() == CorrectAnswerBool.ToString().ToLower())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	public class FillInTheBlankQuestion : Question
+	{
+		public override string QuestionTypeDisplay { get; } = "Fill in the Blank Question";
+		public string CorrectAnswerString { get; set; }
+		public override bool AttemptAnswer(string[] attemptedAnswer)
+		{
+			if (attemptedAnswer[0] == CorrectAnswerString)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	public class OrderQuestion : Question
+	{
+		private class OrderQuestionAnswer
+		{
+			public int CorrectOrder { get; set; }
+			public int RandomOrder { get; set; }
+			public string AnswerString { get; set; }
+		}
+
+		private List<OrderQuestionAnswer> OrderQuestionAnswers { get; set; }
+		public override string QuestionTypeDisplay { get; } = " Order Question ";
+
+		public OrderQuestion(string[] answersInOrder)
+		{
+			OrderQuestionAnswers = new List<OrderQuestionAnswer>();
+			var rand = new Random();
+			for (var i = 0; i < answersInOrder.Length; i++)
+			{
+				var newOrderQuestionAnswer = new OrderQuestionAnswer()
+				{
+					AnswerString = answersInOrder[i],
+					CorrectOrder = i + 1,
+					RandomOrder = rand.Next()
+				};
+				OrderQuestionAnswers.Add(newOrderQuestionAnswer);
+			}
+			OrderQuestionAnswers = OrderQuestionAnswers.OrderBy(oqa => oqa.RandomOrder).ToList();
+			for (int i = 0; i < OrderQuestionAnswers.Count(); i++)
+			{
+				OrderQuestionAnswers[i].RandomOrder = i + 1;
+			}
+
+			NumRequiredAnswers = OrderQuestionAnswers.Count();
+		}
+		public override bool AttemptAnswer(string[] attemptedAnswers)
+		{
+			for (var i = 0; i < attemptedAnswers.Length; i++)
+			{
+				var matchingOrderQuestionAnswer = OrderQuestionAnswers[int.Parse(attemptedAnswers[i]) - 1];
+
+				if (matchingOrderQuestionAnswer.CorrectOrder - 1 != i)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public override List<string> DisplayQuestion()
+		{
+			var returnList = new List<string>()
+			{
+				QuestionString,
+				string.Empty
+			};
+
+			foreach (var orderQuestionAnswer in OrderQuestionAnswers)
+			{
+				returnList.Add(orderQuestionAnswer.RandomOrder + ". " + orderQuestionAnswer.AnswerString);
+			}
+
+			return returnList;
+		}
+
+	}
+
+	public class MultipleChoiceQuestion : Question
+	{
+		public override string QuestionTypeDisplay { get; } = "Multiple Choice Question";
+
+		public string CorrectAnswerString { get; set; }
+
+		public List<string> PossibleAnswers { get; set; }
+
+		public int CorrectAnswerIndex { get; set; }
+
+		public override List<string> DisplayQuestion()
+		{
+			var returnList = new List<string>()
+			{
+				QuestionString,
+				string.Empty
+
+			};
+
+			for(int i = 0; i < PossibleAnswers.Count; i++)
+			{
+				returnList.Add((i+1) + ". " + PossibleAnswers[i]);
+			}
+
+			return returnList;
+		}
+
+		public override bool AttemptAnswer(string[] attemptedAnswer)
+		{
+			if (int.Parse(attemptedAnswer[0]) - 1 == CorrectAnswerIndex)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+	}
+
+
+}
